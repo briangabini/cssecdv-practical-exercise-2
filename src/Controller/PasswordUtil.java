b96dfc6339e0f41f6dcf4b8d076047afa293e259
brngbn <brngbncoding@gmail.com>
2025-06-12 19:08:09 +0800

    feat: add utils for hashing pw

---
+++ src/Controller/PasswordUtil.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtil {
+
+    public static String hash(String plainPassword) { return BCrypt.hashpw(plainPassword, BCrypt.gensalt(12)); }
+
+    public static boolean verify(String plainPassword, String hashedPassword) { return BCrypt.checkpw(plainPassword, hashedPassword); }
+}
